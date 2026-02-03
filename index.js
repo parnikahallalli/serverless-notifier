93ec32277edb22dcf7f6b48b7718147a32acf6ce
parnikahallalli <parnikahallalli@gmail.com>
2026-02-03 20:58:43 +0530

    Add homepage route

--- index.js
+++ index.js
@@ -24,3 +24,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
+app.get("/", (req, res) => {
+    res.send("🚀 Serverless Notifier is LIVE! Send POST requests to /notify");
+  });
+  
